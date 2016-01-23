2f7a3081257fdfcd5e90ac832a958a3fdb8a9ed5
BKDF.NYC <andres.angel@bkdf.nyc>
2016-01-23 15:34:10 -0500

    first push

---
+++ wp-content/themes/popper/template-parts/content.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Template part for displaying posts.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package popper
+ */
+
+?>
+
+<?php
+// Is this the first post of the front page?
+$first_post = $wp_query->current_post == 0 && !is_paged() && is_front_page();
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+	<header class="entry-header">
+		<?php
+		if ( has_post_thumbnail() ) { ?>
+			<figure class="featured-image">
+				<a href="<?php echo esc_url( get_permalink() ); ?>" rel="bookmark">
+					<?php the_post_thumbnail(); ?>
+				</a>
+			</figure>
+		<?php } ?>
+		<?php
+		if ( $first_post == true ) {
+			the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' );
+
+			if ( has_excerpt( $post->ID ) ) {
+				echo '<div class="deck">';
+				echo '<p>' . get_the_excerpt() . '</p>';
+				echo '</div><!-- .deck -->';
+			}
+
+			echo '<div class="entry-meta">';
+			popper_posted_on();
+		} else {
+			the_title( sprintf( '<h2 class="entry-title index-excerpt"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' );
+			echo '<div class="index-entry-meta">';
+			popper_index_posted_on();
+		} ?>
+		</div><!-- .entry-meta -->
+	</header><!-- .entry-header -->
+
+	<?php
+	if ( $first_post == true ) { ?>
+		<div class="entry-content">
+			<?php
+				the_content();
+			?>
+		</div><!-- .entry-content -->
+
+	<?php } else { ?>
+
+		<div class="entry-content index-excerpt">
+			<?php the_excerpt(); ?>
+		</div><!-- .entry-content -->
+
+		<?php echo popper_modify_read_more_link(); ?>
+
+	<?php } ?>
+
+</article><!-- #post-## -->
---
+++ wp-content/themes/smallholder/single.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * The Child Theme template for displaying all single posts.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
+ *
+ * @package popper
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+		<?php while ( have_posts() ) : the_post(); ?>
+
+			<?php get_template_part( 'template-parts/content', 'single' ); ?>
+
+            <?php
+            // Previous/next post navigation.
+            the_post_navigation( array(
+                'next_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Next', 'popper-child' ) . '</span> ' .
+                    '<span class="screen-reader-text">' . __( 'Next post:', 'popper-child' ) . '</span> ' .
+                    '<span class="post-title">%title</span>',
+                'prev_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Previous', 'popper-child' ) . '</span> ' .
+                    '<span class="screen-reader-text">' . __( 'Previous post:', 'popper-child' ) . '</span> ' .
+                    '<span class="post-title">%title</span>',
+            ) );
+            ?>
+
+			<?php
+				// If comments are open or we have at least one comment, load up the comment template.
+				if ( comments_open() || get_comments_number() ) :
+					comments_template();
+				endif;
+			?>
+
+
+
+		<?php endwhile; // End of the loop. ?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
